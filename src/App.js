98a8354c8eaa2af0152ebcc23c06f63eab408560
simicodes <simeonoffiong@gmail.com>
2022-10-28 08:51:43 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import "./App.css";
 import Links from "./Links.js";
 
+
 function App() {
   return (
     <div className="App">
       <div className="header_div">
-        <img id="profile__img" src={require("./profile__img.png")}></img>
+        <img id="profile__img" src={require("./profile__img.png")} alt="profile__img"/>
         <div className="overlay">
-          <img id="ig-icon" src={require("./Icon.png")} />
+          <img id="ig-icon" src={require("./Icon.png")} alt="camera-icon"/>
         </div>
       </div>
-      <img id="share-icon" src={require("./share-icon.png")} />
+      <img id="share-icon" src={require("./share-icon.png")} alt="share-icon"/>
       <div id="mobile_menu">
-        <img id="mobile-btn" src={require("./mobile-btn.png")} />
+        <img id="mobile-btn" src={require("./mobile-btn.png")} alt="mobile-btn"/>
       </div>
 
       <p id="twitter">@simicodes</p>
