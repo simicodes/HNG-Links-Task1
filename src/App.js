002894b572080cfa311d623caabe0c20fb209d08
simicodes <simeonoffiong@gmail.com>
2022-11-04 09:54:05 +0100

    Adding contact button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import "./App.css";
-import Links from "./Links.js";
-
+import Contact from "./Contact";
+import Home from "./Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div className="header_div">
-        <img id="profile__img" src={require("./profile__img.png")} alt="profile__img"/>
-        <div className="overlay">
-          <img id="ig-icon" src={require("./Icon.png")} alt="camera-icon"/>
-        </div>
+    <>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
       </div>
-      <img id="share-icon" src={require("./share-icon.png")} alt="share-icon"/>
-      <div id="mobile_menu">
-        <img id="mobile-btn" src={require("./mobile-btn.png")} alt="mobile-btn"/>
-      </div>
-
-      <p id="twitter">@simicodes</p>
-      <p id="slack">OffiongDev</p>
-      <Links />
-    </div>
+    </>
   );
 }
 
